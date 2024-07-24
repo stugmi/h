8a7bbf0ed2faa4ebfd0f81fa7215ba4fd3218b2d
Smug <me@0day.cat>
2024-07-24 10:52:19 +0200

    h
    hh

---
+++ h.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#define h int
+#define hh ;
+#define hhh main
+#define hhhh (
+#define hhhhh )
+#define hhhhhh {
+#define hhhhhhh }
+#define hhhhhhhh for
+#define hhhhhhhhh <
+#define hhhhhhhhhh >
+#define hhhhhhhhhhh =
+#define hhhhhhhhhhhh ++
+#define hhhhhhhhhhhhh 0
+#define hhhhhhhhhhhhhh 8
+#define hhhhhhhhhhhhhhh 12
+#define hhhhhhhhhhhhhhhh i
+#define hhhhhhhhhhhhhhhhh j
+#define hhhhhhhhhhhhhhhhhh if
+#define hhhhhhhhhhhhhhhhhhh ||
+#define hhhhhhhhhhhhhhhhhhhh &&
+#define hhhhhhhhhhhhhhhhhhhhh 4
+#define hhhhhhhhhhhhhhhhhhhhhh 3
+#define hhhhhhhhhhhhhhhhhhhhhhh std::cout
+#define hhhhhhhhhhhhhhhhhhhhhhhhhh <<
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhh 72
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhh 32
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhh else
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh 10
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh return
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh 0
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh >=
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ==
+#define hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh char
+
+h hhh hhhh hhhhh hhhhhh
+hhhhhhhh hhhh h hhhhhhhhhhhhhhhh hhhhhhhhhhh hhhhhhhhhhhhh hh hhhhhhhhhhhhhhhh hhhhhhhhh hhhhhhhhhhhhhh hh hhhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhh hhhhhh
+hhhhhhhh hhhh h hhhhhhhhhhhhhhhhh hhhhhhhhhhh hhhhhhhhhhhhh hh hhhhhhhhhhhhhhhhh hhhhhhhhh hhhhhhhhhhhhhhh hh hhhhhhhhhhhh hhhhhhhhhhhhhhhhh hhhhh hhhhhh
+hhhhhhhhhhhhhhhhhh hhhh hhhhhhhhhhhhhhhhh hhhhhhhhh hhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhh hhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhh hhhhh hhhhh hhhhhh
+hhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhh hhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhh hh
+hhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhh
+hhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhh hh
+hhhhhhh
+hhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhh hh
+hhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hh
+hhhhhhh
